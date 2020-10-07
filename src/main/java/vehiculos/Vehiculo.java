b6288a364262e72206507658ae2d887ce7692eab
Jurregoz <jonathanurrego@hotmail.com>
2020-10-07 00:09:52 -0500

    Tercer intento

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -127,7 +127,7 @@ public class Vehiculo {
 	}
 	
 	
-	static String vehículosPorTipo() {
+	static String vehiculosPorTipo() {
 		return("Automoviles: " + Automovil.getTotalAutoMovil() +
 				"/n Camionetas: " + Camioneta.getTotalCamionetas() +
 				"/n Camiones: " + Camion.getTotalCamiones()
